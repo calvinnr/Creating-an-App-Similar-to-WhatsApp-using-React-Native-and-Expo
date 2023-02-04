e252cc6e5d4083b31c8c5fb60517ddf658ebc630
calvinnr <vindit54@gmail.com>
2023-02-05 04:09:37 +0700

    UI-4 Chats List

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChatListItem from './src/components/ChatListItem';
-
-const chat = {
-  id: '1',
-  user: {
-    image: 'https://scontent-sin6-3.cdninstagram.com/v/t51.2885-19/306493831_457763942944564_2584794961078525666_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-sin6-3.cdninstagram.com&_nc_cat=104&_nc_ohc=sFRyg8txcpkAX-roeob&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfAcaAKAEEcILplRDs5g2SfXzAIvPd68a1LqP1P4mSwr0Q&oe=63E2CCFA&_nc_sid=6136e7',
-    name: 'Irfan Muhammad',
-  },
-  lastMessage : {
-    text: 'Nongkrong?',
-    createdAt: '04:20',
-  },
-};
+import ChatsScreen from './src/screens/ChatsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatListItem chat={chat} />
+      <ChatsScreen />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -27,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 50,
   },
 });
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Image, Text, View, StyleSheet } from "react-native";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
 
 const ChatListItem = ({ chat }) => {
     return (
@@ -11,7 +15,7 @@ const ChatListItem = ({ chat }) => {
             <View style={styles.content}>
                 <View style={styles.row}>
                     <Text style={styles.name} numberOfLines={1}>{chat.user.name}</Text>
-                    <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
+                    <Text style={styles.subTitle}>{dayjs(chat.lastMessage.createdAt).fromNow(true)}</Text>
                 </View>
 
                 <Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage.text}</Text>
