bc7afe11e7429c0b395654cc63a2012d73fc688a
calvinnr <vindit54@gmail.com>
2023-02-05 03:45:40 +0700

    UI-3 Props

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ChatListItem from './src/components/ChatListItem';
 
+const chat = {
+  id: '1',
+  user: {
+    image: 'https://scontent-sin6-3.cdninstagram.com/v/t51.2885-19/306493831_457763942944564_2584794961078525666_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-sin6-3.cdninstagram.com&_nc_cat=104&_nc_ohc=sFRyg8txcpkAX-roeob&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfAcaAKAEEcILplRDs5g2SfXzAIvPd68a1LqP1P4mSwr0Q&oe=63E2CCFA&_nc_sid=6136e7',
+    name: 'Irfan Muhammad',
+  },
+  lastMessage : {
+    text: 'Nongkrong?',
+    createdAt: '04:20',
+  },
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatListItem />
-      <ChatListItem />
+      <ChatListItem chat={chat} />
       <StatusBar style="auto" />
     </View>
   );
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Image, Text, View, StyleSheet } from "react-native";
 
-const ChatListItem = () => {
+const ChatListItem = ({ chat }) => {
     return (
         <View style={styles.container}>
             <Image 
-            source={{ uri: 'https://scontent-sin6-3.cdninstagram.com/v/t51.2885-19/306493831_457763942944564_2584794961078525666_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-sin6-3.cdninstagram.com&_nc_cat=104&_nc_ohc=sFRyg8txcpkAX-roeob&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfAcaAKAEEcILplRDs5g2SfXzAIvPd68a1LqP1P4mSwr0Q&oe=63E2CCFA&_nc_sid=6136e7' }}
+            source={{ uri: chat.user.image  }}
             style={styles.image}
             />
             <View style={styles.content}>
                 <View style={styles.row}>
-                    <Text style={styles.name} numberOfLines={1}>Irfan Muhammad</Text>
-                    <Text style={styles.subTitle}>04:20</Text>
+                    <Text style={styles.name} numberOfLines={1}>{chat.user.name}</Text>
+                    <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
                 </View>
 
-                <Text numberOfLines={2} style={styles.subTitle}>Nongkrong?</Text>
+                <Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage.text}</Text>
             </View>
         </View>
     );
