137c1d0f795e6ef60890d43de6f7de6834f5eb24
calvinnr <vindit54@gmail.com>
2023-02-05 04:49:49 +0700

    UI-5 Chat Screen

---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,29 @@
+import { View, Text, ImageBackground, FlatList, StyleSheet } from 'react-native';
+import React from 'react';
+import bg from '../../assets/images/BG.png';
+import Message from '../components/Message';
+import messages from '../../assets/data/messages.json';
+
+const ChatScreen = () => {
+  return (
+    <ImageBackground source={bg} style={styles.bg}>
+      <FlatList 
+        data={messages}
+        renderItem={({item}) => <Message message={item} />}
+        style={styles.list}
+        inverted
+      />
+    </ImageBackground>
+  );
+};
+
+export default ChatScreen;
+
+const styles = StyleSheet.create ({
+    bg: {
+        flex: 1,
+    },
+    list: {
+        padding: 10,
+    },
+});
